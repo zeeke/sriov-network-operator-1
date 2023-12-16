d308eaa854ba10fce9c3031a18e19fd294a3085c
Sebastian Sch <sebassch@gmail.com>
2023-12-16 12:45:29 +0200

    global
    Signed-off-by: Sebastian Sch <sebassch@gmail.com>

--- pkg/global/vars/vars.go
+++ pkg/global/vars/vars.go
@@ -11,43 +11,52 @@ import (
 )
 
 var (
+	// ClusterType used by the operator to specify the platform it's running on
+	// supported values [kubernetes,openshift]
 	ClusterType string
 
-	// PlatformsMap contains supported platforms for virtual VF
-	PlatformsMap map[string]consts.PlatformTypes
-
+	// DevMode controls the developer mode in the operator
+	// developer mode allows the operator to use un-supported network devices
 	DevMode bool
 
-	InChroot         bool
-	UsingSystemdMode bool
+	// NodeName initialize and used by the config-daemon to identify the node it's running on
+	NodeName = ""
 
+	// Destdir destination directory for the checkPoint file on the host
 	Destdir string
 
-	NodeName string
+	// PlatformType specify the current platform the operator is running on
+	PlatformType = consts.Baremetal
+	// PlatformsMap contains supported platforms for virtual VF
+	PlatformsMap = map[string]consts.PlatformTypes{
+		"openstack": consts.VirtualOpenStack,
+	}
 
-	PlatformType consts.PlatformTypes
-
-	// FilesystemRoot used by test to mock interactions with filesystem
-	FilesystemRoot string
-
-	PfPhysPortNameRe = regexp.MustCompile(`p\d+`)
-
+	// SupportedVfIds list of supported virtual functions IDs
+	// loaded on daemon initialization by reading the supported-nics configmap
 	SupportedVfIds []string
 
+	// DpdkDrivers supported DPDK drivers for virtual functions
 	DpdkDrivers = []string{"igb_uio", "vfio-pci", "uio_pci_generic"}
 
+	// InChroot global variable to mark that the config-daemon code is inside chroot on the host file system
+	InChroot = false
+
+	// UsingSystemdMode global variable to mark the config-daemon is running on systemd mode
+	UsingSystemdMode = false
+
+	// FilesystemRoot used by test to mock interactions with filesystem
+	FilesystemRoot = ""
+
 	//Cluster variables
-	Config *rest.Config
-	Scheme *runtime.Scheme
+	Config *rest.Config    = nil
+	Scheme *runtime.Scheme = nil
+
+	// PfPhysPortNameRe regex to find switchdev devices on the host
+	PfPhysPortNameRe = regexp.MustCompile(`p\d+`)
 )
 
 func init() {
-	NodeName = ""
-	InChroot = false
-	UsingSystemdMode = false
-	Config = nil
-	Scheme = nil
-
 	ClusterType = os.Getenv("CLUSTER_TYPE")
 
 	DevMode = false
@@ -61,12 +70,4 @@ func init() {
 	if destdir != "" {
 		Destdir = destdir
 	}
-
-	FilesystemRoot = ""
-
-	PlatformsMap = map[string]consts.PlatformTypes{
-		"openstack": consts.VirtualOpenStack,
-	}
-
-	PlatformType = consts.Baremetal
 }
