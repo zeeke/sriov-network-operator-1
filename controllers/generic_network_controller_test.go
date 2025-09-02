k8snetworkplumbingwg/sriov-network-operator

networks: Add `Owner` annottion to NetAttachDefs
NetworkattachmentDefiitions are usually created in namespaces that are different
than the Sriov{Ib,OVS}Networks, so the OwnerReference object field can't be
used to endorse the ownership reference.

Add a `sriovnetwork.openshift.io/owner` annotation to put on the NetAttachDef
to avoid multiple {Sriov,SriovIb,OVS}Network objects does not override changes.

Signed-off-by: Andrea Panattoni <apanatto@redhat.com>

controllers/generic_network_controller_test.go
@@ -1,0 +1,187 @@
+package controllers
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	netattdefv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	sriovnetworkv1 "github.com/k8snetworkplumbingwg/sriov-network-operator/api/v1"
+	"github.com/k8snetworkplumbingwg/sriov-network-operator/pkg/consts"
+	"github.com/k8snetworkplumbingwg/sriov-network-operator/test/util"
+)
+
+var _ = Describe("All Network Controllers", Ordered, func() {
+	var cancel context.CancelFunc
+	var ctx context.Context
+
+	BeforeAll(func() {
+		By("Setup controller manager")
+		k8sManager, err := setupK8sManagerForTest()
+		Expect(err).ToNot(HaveOccurred())
+
+		err = (&SriovNetworkReconciler{
+			Client: k8sManager.GetClient(),
+			Scheme: k8sManager.GetScheme(),
+		}).SetupWithManager(k8sManager)
+		Expect(err).ToNot(HaveOccurred())
+
+		err = (&SriovIBNetworkReconciler{
+			Client: k8sManager.GetClient(),
+			Scheme: k8sManager.GetScheme(),
+		}).SetupWithManager(k8sManager)
+		Expect(err).ToNot(HaveOccurred())
+
+		err = (&OVSNetworkReconciler{
+			Client: k8sManager.GetClient(),
+			Scheme: k8sManager.GetScheme(),
+		}).SetupWithManager(k8sManager)
+		Expect(err).ToNot(HaveOccurred())
+
+		ctx, cancel = context.WithCancel(context.Background())
+
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			defer GinkgoRecover()
+			By("Start controller manager")
+			err := k8sManager.Start(ctx)
+			Expect(err).ToNot(HaveOccurred())
+		}()
+
+		DeferCleanup(func() {
+			By("Shutdown controller manager")
+			cancel()
+			wg.Wait()
+		})
+	})
+
+	Context("owner-reference annotations", func() {
+		AfterEach(func() {
+			k8sClient.DeleteAllOf(ctx, &sriovnetworkv1.SriovNetwork{}, client.InNamespace(testNamespace))
+			k8sClient.DeleteAllOf(ctx, &sriovnetworkv1.SriovIBNetwork{}, client.InNamespace(testNamespace))
+			k8sClient.DeleteAllOf(ctx, &sriovnetworkv1.OVSNetwork{}, client.InNamespace(testNamespace))
+			k8sClient.DeleteAllOf(ctx, &netattdefv1.NetworkAttachmentDefinition{}, client.InNamespace("default"))
+		})
+
+		It("applies to new netattachdef", func() {
+			cr := sriovnetworkv1.SriovNetwork{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sriovnet-blue",
+					Namespace: testNamespace,
+				},
+				Spec: sriovnetworkv1.SriovNetworkSpec{
+					NetworkNamespace: "default",
+				},
+			}
+
+			err := k8sClient.Create(ctx, &cr)
+			Expect(err).NotTo(HaveOccurred())
+
+			netAttDef := &netattdefv1.NetworkAttachmentDefinition{}
+			err = util.WaitForNamespacedObject(netAttDef, k8sClient, "default", cr.GetName(), util.RetryInterval, util.Timeout)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(netAttDef.GetAnnotations()).To(HaveKeyWithValue(consts.OwnerAnnotation, "SriovNetwork.sriovnetwork.openshift.io/openshift-sriov-network-operator/sriovnet-blue"))
+		})
+
+		It("should migrate existing NetAttachDef to have a value for Owner annotation", func() {
+			netAttachDef := netattdefv1.NetworkAttachmentDefinition{
+				ObjectMeta: metav1.ObjectMeta{Name: "netuser", Namespace: "default"},
+				Spec: netattdefv1.NetworkAttachmentDefinitionSpec{
+					Config: "user created configration, to be overridden",
+				},
+			}
+			err := k8sClient.Create(ctx, &netAttachDef)
+			Expect(err).NotTo(HaveOccurred())
+
+			cr := sriovnetworkv1.SriovNetwork{
+				ObjectMeta: metav1.ObjectMeta{Name: "netuser", Namespace: testNamespace},
+				Spec:       sriovnetworkv1.SriovNetworkSpec{NetworkNamespace: "default"},
+			}
+			err = k8sClient.Create(ctx, &cr)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(func(g Gomega) {
+				netAttDef := &netattdefv1.NetworkAttachmentDefinition{}
+				err = util.WaitForNamespacedObject(netAttDef, k8sClient, "default", cr.GetName(), util.RetryInterval, util.Timeout)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(netAttDef.GetAnnotations()).
+					To(HaveKeyWithValue(consts.OwnerAnnotation, "SriovNetwork.sriovnetwork.openshift.io/openshift-sriov-network-operator/netuser"))
+				g.Expect(netAttDef.Spec.Config).To(Equal(generateExpectedNetConfig(&cr)))
+			}).WithPolling(30 * time.Millisecond).WithTimeout(300 * time.Millisecond).Should(Succeed())
+		})
+
+		Context("does not override the NetAttachDefinition if the Owner annotation does not match", func() {
+			It("when using different network type with the same name", func() {
+				cr := sriovnetworkv1.SriovNetwork{
+					ObjectMeta: metav1.ObjectMeta{Name: "net1", Namespace: testNamespace},
+					Spec:       sriovnetworkv1.SriovNetworkSpec{NetworkNamespace: "default"},
+				}
+				err := k8sClient.Create(ctx, &cr)
+				Expect(err).NotTo(HaveOccurred())
+
+				netAttDef := &netattdefv1.NetworkAttachmentDefinition{}
+				err = util.WaitForNamespacedObject(netAttDef, k8sClient, "default", cr.GetName(), util.RetryInterval, util.Timeout)
+				Expect(err).NotTo(HaveOccurred())
+
+				cr2 := sriovnetworkv1.SriovIBNetwork{
+					ObjectMeta: metav1.ObjectMeta{Name: "net1", Namespace: testNamespace},
+					Spec:       sriovnetworkv1.SriovIBNetworkSpec{NetworkNamespace: "default"},
+				}
+				err = k8sClient.Create(ctx, &cr2)
+				Expect(err).NotTo(HaveOccurred())
+
+				Consistently(func(g Gomega) {
+					netAttDef := &netattdefv1.NetworkAttachmentDefinition{}
+					err = util.WaitForNamespacedObject(netAttDef, k8sClient, "default", cr.GetName(), util.RetryInterval, util.Timeout)
+					g.Expect(err).NotTo(HaveOccurred())
+					g.Expect(netAttDef.Spec.Config).To(ContainSubstring(`"sriov"`))
+					g.Expect(netAttDef.Spec.Config).ToNot(ContainSubstring(`"ib-sriov"`))
+				}).WithPolling(30 * time.Millisecond).WithTimeout(300 * time.Millisecond).Should(Succeed())
+			})
+
+			It("when using the same network type with the same name, in different namespaces", func() {
+				cr := sriovnetworkv1.SriovNetwork{
+					ObjectMeta: metav1.ObjectMeta{Name: "net2", Namespace: testNamespace},
+					Spec: sriovnetworkv1.SriovNetworkSpec{
+						NetworkNamespace: "default",
+						MinTxRate:        ptr.To(42),
+					},
+				}
+				err := k8sClient.Create(ctx, &cr)
+				Expect(err).NotTo(HaveOccurred())
+
+				netAttDef := &netattdefv1.NetworkAttachmentDefinition{}
+				err = util.WaitForNamespacedObject(netAttDef, k8sClient, "default", cr.GetName(), util.RetryInterval, util.Timeout)
+				Expect(err).NotTo(HaveOccurred())
+
+				cr2 := sriovnetworkv1.SriovNetwork{
+					ObjectMeta: metav1.ObjectMeta{Name: "net2", Namespace: "default"},
+					Spec: sriovnetworkv1.SriovNetworkSpec{
+						MinTxRate: ptr.To(84),
+					},
+				}
+				err = k8sClient.Create(ctx, &cr2)
+				Expect(err).NotTo(HaveOccurred())
+
+				Consistently(func(g Gomega) {
+					netAttDef := &netattdefv1.NetworkAttachmentDefinition{}
+					err = util.WaitForNamespacedObject(netAttDef, k8sClient, "default", cr.GetName(), util.RetryInterval, util.Timeout)
+					g.Expect(err).NotTo(HaveOccurred())
+					g.Expect(netAttDef.Spec.Config).To(ContainSubstring(`"min_tx_rate": 42`))
+					g.Expect(netAttDef.Spec.Config).ToNot(ContainSubstring(`"min_tx_rate": 84`))
+				}).WithPolling(30 * time.Millisecond).WithTimeout(300 * time.Millisecond).Should(Succeed())
+			})
+		})
+	})
+
+})

zeeke/sriov-network-operator-1

controllers/generic_network_controller_test.go
@@ -1,0 +1,187 @@
+package controllers
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	netattdefv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	sriovnetworkv1 "github.com/k8snetworkplumbingwg/sriov-network-operator/api/v1"
+	"github.com/k8snetworkplumbingwg/sriov-network-operator/pkg/consts"
+	"github.com/k8snetworkplumbingwg/sriov-network-operator/test/util"
+)
+
+var _ = Describe("All Network Controllers", Ordered, func() {
+	var cancel context.CancelFunc
+	var ctx context.Context
+
+	BeforeAll(func() {
+		By("Setup controller manager")
+		k8sManager, err := setupK8sManagerForTest()
+		Expect(err).ToNot(HaveOccurred())
+
+		err = (&SriovNetworkReconciler{
+			Client: k8sManager.GetClient(),
+			Scheme: k8sManager.GetScheme(),
+		}).SetupWithManager(k8sManager)
+		Expect(err).ToNot(HaveOccurred())
+
+		err = (&SriovIBNetworkReconciler{
+			Client: k8sManager.GetClient(),
+			Scheme: k8sManager.GetScheme(),
+		}).SetupWithManager(k8sManager)
+		Expect(err).ToNot(HaveOccurred())
+
+		err = (&OVSNetworkReconciler{
+			Client: k8sManager.GetClient(),
+			Scheme: k8sManager.GetScheme(),
+		}).SetupWithManager(k8sManager)
+		Expect(err).ToNot(HaveOccurred())
+
+		ctx, cancel = context.WithCancel(context.Background())
+
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			defer GinkgoRecover()
+			By("Start controller manager")
+			err := k8sManager.Start(ctx)
+			Expect(err).ToNot(HaveOccurred())
+		}()
+
+		DeferCleanup(func() {
+			By("Shutdown controller manager")
+			cancel()
+			wg.Wait()
+		})
+	})
+
+	Context("owner-reference annotations", func() {
+		AfterEach(func() {
+			k8sClient.DeleteAllOf(ctx, &sriovnetworkv1.SriovNetwork{}, client.InNamespace(testNamespace))
+			k8sClient.DeleteAllOf(ctx, &sriovnetworkv1.SriovIBNetwork{}, client.InNamespace(testNamespace))
+			k8sClient.DeleteAllOf(ctx, &sriovnetworkv1.OVSNetwork{}, client.InNamespace(testNamespace))
+			k8sClient.DeleteAllOf(ctx, &netattdefv1.NetworkAttachmentDefinition{}, client.InNamespace("default"))
+		})
+
+		It("applies to new netattachdef", func() {
+			cr := sriovnetworkv1.SriovNetwork{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sriovnet-blue",
+					Namespace: testNamespace,
+				},
+				Spec: sriovnetworkv1.SriovNetworkSpec{
+					NetworkNamespace: "default",
+				},
+			}
+
+			err := k8sClient.Create(ctx, &cr)
+			Expect(err).NotTo(HaveOccurred())
+
+			netAttDef := &netattdefv1.NetworkAttachmentDefinition{}
+			err = util.WaitForNamespacedObject(netAttDef, k8sClient, "default", cr.GetName(), util.RetryInterval, util.Timeout)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(netAttDef.GetAnnotations()).To(HaveKeyWithValue(consts.OwnerRefAnnotation, "SriovNetwork.sriovnetwork.openshift.io/openshift-sriov-network-operator/sriovnet-blue"))
+		})
+
+		It("should migrate existing NetAttachDef to have a value for Owner annotation", func() {
+			netAttachDef := netattdefv1.NetworkAttachmentDefinition{
+				ObjectMeta: metav1.ObjectMeta{Name: "netuser", Namespace: "default"},
+				Spec: netattdefv1.NetworkAttachmentDefinitionSpec{
+					Config: "user created configration, to be overridden",
+				},
+			}
+			err := k8sClient.Create(ctx, &netAttachDef)
+			Expect(err).NotTo(HaveOccurred())
+
+			cr := sriovnetworkv1.SriovNetwork{
+				ObjectMeta: metav1.ObjectMeta{Name: "netuser", Namespace: testNamespace},
+				Spec:       sriovnetworkv1.SriovNetworkSpec{NetworkNamespace: "default"},
+			}
+			err = k8sClient.Create(ctx, &cr)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(func(g Gomega) {
+				netAttDef := &netattdefv1.NetworkAttachmentDefinition{}
+				err = util.WaitForNamespacedObject(netAttDef, k8sClient, "default", cr.GetName(), util.RetryInterval, util.Timeout)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(netAttDef.GetAnnotations()).
+					To(HaveKeyWithValue(consts.OwnerRefAnnotation, "SriovNetwork.sriovnetwork.openshift.io/openshift-sriov-network-operator/netuser"))
+				g.Expect(netAttDef.Spec.Config).To(Equal(generateExpectedNetConfig(&cr)))
+			}).WithPolling(30 * time.Millisecond).WithTimeout(300 * time.Millisecond).Should(Succeed())
+		})
+
+		Context("does not override the NetAttachDefinition if the Owner annotation does not match", func() {
+			It("when using different network type with the same name", func() {
+				cr := sriovnetworkv1.SriovNetwork{
+					ObjectMeta: metav1.ObjectMeta{Name: "net1", Namespace: testNamespace},
+					Spec:       sriovnetworkv1.SriovNetworkSpec{NetworkNamespace: "default"},
+				}
+				err := k8sClient.Create(ctx, &cr)
+				Expect(err).NotTo(HaveOccurred())
+
+				netAttDef := &netattdefv1.NetworkAttachmentDefinition{}
+				err = util.WaitForNamespacedObject(netAttDef, k8sClient, "default", cr.GetName(), util.RetryInterval, util.Timeout)
+				Expect(err).NotTo(HaveOccurred())
+
+				cr2 := sriovnetworkv1.SriovIBNetwork{
+					ObjectMeta: metav1.ObjectMeta{Name: "net1", Namespace: testNamespace},
+					Spec:       sriovnetworkv1.SriovIBNetworkSpec{NetworkNamespace: "default"},
+				}
+				err = k8sClient.Create(ctx, &cr2)
+				Expect(err).NotTo(HaveOccurred())
+
+				Consistently(func(g Gomega) {
+					netAttDef := &netattdefv1.NetworkAttachmentDefinition{}
+					err = util.WaitForNamespacedObject(netAttDef, k8sClient, "default", cr.GetName(), util.RetryInterval, util.Timeout)
+					g.Expect(err).NotTo(HaveOccurred())
+					g.Expect(netAttDef.Spec.Config).To(ContainSubstring(`"sriov"`))
+					g.Expect(netAttDef.Spec.Config).ToNot(ContainSubstring(`"ib-sriov"`))
+				}).WithPolling(30 * time.Millisecond).WithTimeout(300 * time.Millisecond).Should(Succeed())
+			})
+
+			It("when using the same network type with the same name, in different namespaces", func() {
+				cr := sriovnetworkv1.SriovNetwork{
+					ObjectMeta: metav1.ObjectMeta{Name: "net2", Namespace: testNamespace},
+					Spec: sriovnetworkv1.SriovNetworkSpec{
+						NetworkNamespace: "default",
+						MinTxRate:        ptr.To(42),
+					},
+				}
+				err := k8sClient.Create(ctx, &cr)
+				Expect(err).NotTo(HaveOccurred())
+
+				netAttDef := &netattdefv1.NetworkAttachmentDefinition{}
+				err = util.WaitForNamespacedObject(netAttDef, k8sClient, "default", cr.GetName(), util.RetryInterval, util.Timeout)
+				Expect(err).NotTo(HaveOccurred())
+
+				cr2 := sriovnetworkv1.SriovNetwork{
+					ObjectMeta: metav1.ObjectMeta{Name: "net2", Namespace: "default"},
+					Spec: sriovnetworkv1.SriovNetworkSpec{
+						MinTxRate: ptr.To(84),
+					},
+				}
+				err = k8sClient.Create(ctx, &cr2)
+				Expect(err).NotTo(HaveOccurred())
+
+				Consistently(func(g Gomega) {
+					netAttDef := &netattdefv1.NetworkAttachmentDefinition{}
+					err = util.WaitForNamespacedObject(netAttDef, k8sClient, "default", cr.GetName(), util.RetryInterval, util.Timeout)
+					g.Expect(err).NotTo(HaveOccurred())
+					g.Expect(netAttDef.Spec.Config).To(ContainSubstring(`"min_tx_rate": 42`))
+					g.Expect(netAttDef.Spec.Config).ToNot(ContainSubstring(`"min_tx_rate": 84`))
+				}).WithPolling(30 * time.Millisecond).WithTimeout(300 * time.Millisecond).Should(Succeed())
+			})
+		})
+	})
+
+})